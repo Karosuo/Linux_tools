c22bacc4eb20e411eff376343de9d8c87951e814
Rafael_Karosuo <rafaelkarosuo@gmail.com>
2016-08-14 19:50:08 -0700

    Structure complete, beta testing

--- doBackup.sh
+++ doBackup.sh
@@ -1,20 +1,56 @@
 #!/bin/bash
 
-if [ -z "$1" -o -z "$2" ]; then
- echo -e "\nusage: $0 <source list file> <destiny list file>\n"
- echo -e "\tsource list file: File with a new line separated list of source paths"
- echo -e "\tdestiny list file: File with a new line separated list of destiny paths"
- echo -e "\n"
+#by Rafael Karosuo (rafaelkarsuo@gmail.com)
+
+#Performs a backup using rsync
+	#Parameters are:
+	#If the destiny is compressed
+	#Source list of paths
+	#Destiny list of paths
+
+#It could be multiple directories as source and/or as destiny
+#but at least must be one of each
+
+#Options used from rsync:
+	#-a: archive
+	#-H: maintain hard links
+	#-A: preserve ACL's (Access Control Lists)
+	#-X: keeps xattrs
+	#-v: verbose
+	#-z: compress
+
+#Files generated are:
+	#actionLog: Show's all the output from rsync
+	#errLog: Has any error from rsync
+	#timeLog: Has the copy time of each file
+
+if [ "$1" = "--compressed" ]; then
+	source="$2"
+	destiny="$3"
+	params="-aHAXvz"
 else
- while read destiny #Read destiny list
- do
+	source="$1"
+	destiny="$2"
+	params="-aHAXv"
+fi
 
-  while read source #Read source list
-  do
-   echo "---------Action: rsync -aHAXvz $source $destiny"
-   #rsync $line $2 > ActionLog 2> errLog
-  done<"$1"
+if [ -z "$source" -o -z "$destiny" ]; then
+	echo -e "\nusage: $0 [--compressed] <source list file> <destiny list file>\n"
+	echo -e "\tsource list file: File with a new line separated list of source paths"
+	echo -e "\tdestiny list file: File with a new line separated list of destiny paths"
+	echo -e "\n"
+else
 
- done<"$2"
+	while read destinyPath #Read destiny list
+	do
+		while read sourcePath #Read source list
+		do
+			actionTitle="---------Action: rsync $params $sourcePath $destinyPath"
+			echo -e "\n\n$actionTitle" >> timeLog
+			echo -e "\n\n$actionTitle" >> errLog
+			(time rsync "$params" "$sourcePath" "$destinyPath" > actionLog 2> errLog) &>> timeLog
+		done<"$source"
+
+	done<"$destiny"
 
 fi
