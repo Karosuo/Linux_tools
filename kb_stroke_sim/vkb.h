0ae21683ae9ff86dcb9962ced44fb59f404e7202
Rafael Karosuo <rafaelkarosuo@gmail.com>
2017-04-10 11:04:43 -0700

    change the WINVER to target win 7 instead of win2000

--- kb_stroke_sim/vkb.h
+++ kb_stroke_sim/vkb.h
@@ -1,8 +1,6 @@
 #ifdef __linux__ /**Could be __linux, linux or __linux__*/
-#elif _WIN32
-	//~ As in the Ted Burke example, set WINVER shold be setted to win 2000
-	#define WINVER 0x0500
-	//~ #define WINVER 0x0601 //For win 7
+#elif _WIN32	
+	#define WINVER 0x0601 //For win 7
 	#include <windows.h>	
 #else
 #error "Not targeted OS!"
