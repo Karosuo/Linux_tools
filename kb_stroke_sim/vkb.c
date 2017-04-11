e8029c956c3dc27e0437bdba35ebda532aee694f
Rafael_Karosuo <rafaelkarosuo@gmail.com>
2017-04-10 22:27:22 -0700

    Test, trying to get xdotool to work in ubuntu, but no xdo_type funct found, so tried to paste separate implementations, but it didn't work

--- kb_stroke_sim/vkb.c
+++ kb_stroke_sim/vkb.c
@@ -2,7 +2,7 @@
  *  Independent module that receives a string as parameter and send it to the OS as virtual keyboard
  * 	The characters as expected as basic ASCII, no accents or other unicode characters tested, but sendInput in windows support thouse tho
  * 	
- * 	It supports Windows systems from 2000, in 32 bit systems (which also run in x64), as well as linux x64 versions only
+ * 	It supports Windows systems from 2000, in 32 bit systems (which also run in x64), as well as ubuntu x64 versions only using xdotool
  * 
  * 	The parameters can be passed as command line ones, in the project is used by a python script with the following format
  * 	from subprocess import call
@@ -10,6 +10,8 @@
  * 
  * 	Part of a project in UABC that needed a virtual kb interface, FCQI, Computer Engineering 2017-1
  * 	date: apr - 9 - 17
+ * 
+ * 	For the linux part, install libxdo "sudo apt-get install libxdo-dev libxdo2", since xdotool is a bash tool not a C library
  * 
  * 	Some useful reference:
  * 		KEYBDINPUT structure - https://msdn.microsoft.com/en-us/library/windows/desktop/ms646271(v=vs.85).aspx
@@ -19,6 +21,8 @@
  * 		doxygen comment style - https://www.stack.nl/~dimitri/doxygen/manual/docblocks.html
  * 		WINVER number representations - https://msdn.microsoft.com/en-us/library/6sehtctf.aspx
  * 		char to virtual key translator funct (VkKeyScanEx) - https://msdn.microsoft.com/en-us/library/windows/desktop/ms646332(v=vs.85).aspx
+ * 
+ * 		xdotool online ref - http://www.semicomplete.com/files/xdotool/docs/html/xdo_8h.html
  * 	
  * 	By Rafael Karosuo rafaelkarosuo@gmail.com
  * */
@@ -33,14 +37,24 @@
  * Both return int as a status
  * */
 #ifdef __linux__
-	/// Brief wrapper function to simulate vkb on linux systems
+	/// Brief wrapper function to simulate vkb on ubuntu systems using xdotool
+	/**
+	 * @param c_key is the character to print using keyboard simulation
+	 * */
 	int kb_print_char(char key)
 	{
-		printf("\nHello World Linux + char: %c\n", key);
-		return 0;
+		char key_wrapper[1]; ///< xdo_type needs a pointer, this is to be able to keep the transparent interface of kb_print_char
+		*key_wrapper = key;
+		xdo_t * xdo_instance = xdo_new(NULL); ///< Create new xdo instance and null param indicates that uses env var DISPLAY, also could be used ":0.0"
+		xdo_type(xdo_instance, CURRENTWINDOW, key_wrapper, 0); ///< Send a string to the current window, check xdotool ref
+		return 1; ///< As windows sendinput return 0 means error, we need to return the number of correct succesful insertions
 	}
+
 #elif _WIN32
 	/// Brief wrapper function to simulate vkb on windows systems
+	/**
+	 * @param c_key is the character to print using keyboard simulation
+	 * */
 	int kb_print_char(char c_key)
 	{
 		/*** 0x0409 is the US english layout, KLF_ACTIVATE is load the layout if not loaded */
@@ -49,26 +63,28 @@
 		INPUT in_struct; ///< Declare an INPUT structure, to know which type will be, mouse, kb or other hdw
 		in_struct.type = INPUT_KEYBOARD; ///< INPUT_KEYBOARD is an enum (num 1) and should be combined with "ki" struct
 		in_struct.ki.time = 0; ///< 0 means let the OS put it's own time stamp
-		in_struct.ki.wVk = v_key; // virtual-key code, use the sent char param
-		in_struct.ki.dwFlags = 0; // 0 for key press
+		in_struct.ki.wVk = v_key; ///< virtual-key code, use the sent char param
+		in_struct.ki.dwFlags = 0; ///< 0 for key press
 		in_status = SendInput(1, &in_struct, sizeof(INPUT)); ///< Indicates only 1 input
 		if(in_status == 0)
 			return 0; ///< don't try to lift the key since resource is blocked
-		in_struct.ki.dwFlags = KEYEVENTF_KEYUP; // KEYEVENTF_KEYUP for key release
+		in_struct.ki.dwFlags = KEYEVENTF_KEYUP; ///< KEYEVENTF_KEYUP for key release
 		SendInput(1, &in_struct, sizeof(INPUT));
-		return in_status; ///< return the number of succesfull insertions
-	}
-	
-	/// Brief Wrapper of the kb_print_char function, to be able to print strings in a faster way
-	int kb_print_str(char * str)
-	{
-		Sleep(7000);
-		while(*str)///< Iterates over the array, as it's null terminated
-		{
-			kb_print_char(*str++); ///< Point to the current address, get's it's content and after that, adds the address
-		}
+		return in_status; ///< return the number of succesful insertions
 	}
 #endif
+
+/// Brief Wrapper of the kb_print_char function, to be able to print strings in a faster way
+/**
+ * @param *str is the not constant char pointer to the string that will be printed using the kb simulation
+ * */
+int kb_print_str(char * str)
+{
+	while(*str)///< Iterates over the array, as it's null terminated
+	{
+		kb_print_char(*str++); ///< Point to the current address, get's it's content and after that, adds the address
+	}
+}
 
 
 int main(int argc, char * argv[])
--- kb_stroke_sim/vkb.h
+++ kb_stroke_sim/vkb.h
@@ -1,4 +1,5 @@
 #ifdef __linux__ /**Could be __linux, linux or __linux__*/
+	#include <xdo.h> ///<For xdotool
 #elif _WIN32	
 	#define WINVER 0x0601 //For win 7
 	#include <windows.h>	
@@ -8,6 +9,6 @@
 
 /// Brief wrapper function to print a string using kb_print_char
 int kb_print_str(char * str);
-/// Brief single call to sendInput or xxx, in windows or linux systems respectively
+/// Brief single call to sendInput or xxx, in windows or ubuntu systems using xdotool respectively
 /// Details Only simulates a hit and release of a character key, doesn't support the long press or combinations
 int kb_print_char(char key);
